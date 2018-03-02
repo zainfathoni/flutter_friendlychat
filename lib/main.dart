01cd0829858ddf7af677e7fee403362389de0f47
Zain Fathoni <zain.fathoni@gmail.com>
2018-03-02 09:47:47 +0800

    Add a responsive Send button

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,30 @@ class ChatScreenState extends State<ChatScreen> {
   }
 
   Widget _buildTextComposer() {
-    return new Container(
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: new TextField(
-          controller: _textController,
-          onSubmitted: _handleSubmitted,
-          decoration: new InputDecoration.collapsed(hintText: "Send a message"),
-        ));
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   void _handleSubmitted(String text) {
